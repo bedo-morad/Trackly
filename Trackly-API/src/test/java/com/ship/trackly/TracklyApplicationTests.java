055478b1d2528b9b31ec24df19c0d5873feb01b6
Abdalrahman Mosad <bodymosad67@gmail.com>
2026-08-16 14:24:51 +0300

    init project

---
+++ Trackly-API/src/test/java/com/ship/trackly/TracklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ship.trackly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracklyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
